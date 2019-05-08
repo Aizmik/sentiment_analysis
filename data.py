4a2980bc4d16b32b1e441a05c3d52665e3bbf9ac
Aizmik <blek_1990@list.ru>
2019-05-08 20:21:48 +0300

    Restartet project, to increase accuracy by implementing neural networkRestartet project, to increase accuracy by implementing neural network

---
+++ data.py
@@ -0,0 +1,52 @@
+import re
+import pymorphy2
+import pandas as pd
+from nltk import word_tokenize
+from sklearn.utils import shuffle
+from gensim.models import FastText
+
+
+def read_data(path):
+    return pd.read_csv(path, error_bad_lines=False,
+                       warn_bad_lines=False, header=None, sep=';')
+
+
+def get_vector(sentence):
+    FT_model = FastText.load(r'models\fasttext.model')
+    sentence = re.findall('[а-яё]+',  str(word_tokenize(sentence.lower())))
+    vector = []
+    morph = pymorphy2.MorphAnalyzer()
+
+    for word in sentence:
+        word = morph.parse(word)[0].normal_form
+        try:
+            if vector:
+                vector += FT_model[word]
+                continue
+
+            vector = FT_model[word]
+
+        except(Exception):
+            pass
+
+    return vector
+
+
+def get_data():
+    positive = read_data('positive.csv')
+    negative = read_data('negative.csv')
+
+    x = []
+    y = []
+
+    for tweet in positive[:][3]:
+        x.append(get_vector(tweet))
+        y.append(0)
+
+    for tweet in negative[:][3]:
+        x.append(get_vector(tweet))
+        y.append(1)
+
+    x, y = shuffle(x, y)
+
+    return x, y
